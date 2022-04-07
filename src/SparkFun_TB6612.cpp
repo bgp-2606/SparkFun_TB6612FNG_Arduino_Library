17fdd380d7a5cc1bfc37f31cdd44d86a8db20a74
Bren-Gelyn Padlan <brengelynp@gmail.com>
2022-04-07 03:29:58 -0400

    Modified speed values

--- src/SparkFun_TB6612.cpp
+++ src/SparkFun_TB6612.cpp
@@ -77,7 +77,7 @@ void Motor::standby()
 
 void forward(Motor motor1, Motor motor2, int speed)
 {
-	motor1.drive(speed + 50);
+	motor1.drive(speed + 45);
 	motor2.drive(speed);
 }
 void forward(Motor motor1, Motor motor2)
@@ -90,7 +90,7 @@ void forward(Motor motor1, Motor motor2)
 void back(Motor motor1, Motor motor2, int speed)
 {
 	int temp = abs(speed);
-	int temp2 = abs(speed + 50);
+	int temp2 = abs(speed + 45);
 	motor1.drive(-temp2);
 	motor2.drive(-temp);
 }
@@ -102,7 +102,7 @@ void back(Motor motor1, Motor motor2)
 void left(Motor left, Motor right, int speed)
 {
 	int temp = abs(speed)/2;
-	int temp2 = abs(speed)/2 + 60;
+	int temp2 = abs(speed)/2 + 45;
 	left.drive(-temp2);
 	right.drive(temp);
 	
@@ -112,7 +112,7 @@ void left(Motor left, Motor right, int speed)
 void right(Motor left, Motor right, int speed)
 {
 	int temp = abs(speed)/2;
-	int temp2 = abs(speed)/2 + 60;
+	int temp2 = abs(speed)/2 + 45;
 	left.drive(temp2);
 	right.drive(-temp);
 	
